1c32c9910e91bc8d8d56b76254d02c97f29217bb
Aaron Broussard <abroussard11@gmail.com>
2014-10-10 15:17:38 -0500

    Got the CMake build scheme working properly\! Yay\!

--- src/Main/Main.cpp
+++ src/Main/Main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-//#include "Fish.h"
+#include <FishGroup/Fish.h>
 
 int main()
 {
    std::cout << "Hello World!" << std::endl;
-//   FishGroup::Fish myFish;
-//   myFish.swim();
+   FishGroup::Fish myFish;
+   myFish.swim();
 }
