01c9c83db8908827fb66e6a38d78126eb4c134d8
Aaron Broussard <abroussard11@gmail.com>
2017-12-21 01:54:26 -0700

    add first haskell code

---
+++ haskell/app/Main.hs
@@ -0,0 +1,21 @@
+module Main where
+
+import           Bindings
+import           Data.IORef
+import           Graphics.UI.GLUT
+import           Lib
+
+main :: IO ()
+main = do
+  (_progName, _args) <- getArgsAndInitialize
+  initialDisplayMode $= [WithDepthBuffer, DoubleBuffered]
+  _window <- createWindow "Hello World"
+  reshapeCallback $= Just reshape
+  depthFunc $= Just Less
+  angle <- newIORef 0
+  delta <- newIORef 0.1
+  pos <- newIORef (0, 0)
+  keyboardMouseCallback $= Just (keyboardMouse delta pos)
+  idleCallback $= Just (idle angle delta)
+  displayCallback $= display angle pos
+  mainLoop
